e79d92831bae1e5d270e235f58eb2e1de8538341
devolisto <devolisto@gmail.com>
2021-01-23 19:02:03 +0300

    ...

--- bareed.js
+++ bareed.js
@@ -1,27 +1,24 @@
-/**************************************************************
- * Point: defines a point on the map using X and Y coordinates
- *
- * x: x coordinate
- * y: y coordinate
- *
- * distanceTo(point): takes a point, calculates the distance to
- *                     that point from the current point.
- *
- * let point = new Point(x, y);
- ****************************************************************/
+// 
+/***********************************************************
+ * If you want examples of how to use the
+ * these classes and how to test your code manually,
+ * check out the README.md file
+ ***********************************************************/
+
+
 class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
-  distanceTo = point => {
+  distanceTo = (point) => {
     let xDelta = this.x - point.x;
     let yDelta = this.y - point.y;
     return Math.sqrt(xDelta * xDelta + yDelta * yDelta); // PYTHAGORAS!
   };
 
-  equals = point => point.x === this.x && point.y === this.y;
+  equals = (point) => point.x === this.x && point.y === this.y;
 
   static randomPoint = (maxX, maxY) => {
     let x = Math.random() * (maxX || 100);
@@ -47,12 +44,12 @@ class Wallet {
     this.money = money;
   }
 
-  credit = amount => {
-    let moreMoney = this.money += amount ;
+  credit = (amount) => {
+    this.money += amount;
   };
 
-  debit = amount => {
-    let deducts = this.money -= amount;
+  debit = (amount) => {
+    this.money -= amount;
   };
 }
 
@@ -60,7 +57,7 @@ class Wallet {
  * Person: defines a person with a name (and feelings)
  *
  * name: name of said person
- * location: a Point
+ * location: a Point instance
  * wallet: a Wallet instance initially with 0.
  *
  * moveTo(point): updates the `location` to `point`
@@ -68,17 +65,15 @@ class Wallet {
  * let person = new Person(name, x, y);
  **********************************************************/
 class Person {
-  // implement Person!
-  constructor(name, x, y){
+  constructor(name, x, y) {
     this.name = name;
-    // this.point = new Point(x,y);
-    // this.wallet = new Wallet(money=0);
+    this.location = new Point(x, y);
+    this.wallet = new Wallet();
   }
 
-  // locaton = new Point() 
-
-  // wallet = new Wallet(money = 0);
-
+  moveTo = (point) => {
+    this.location = point;
+  };
 }
 
 /**********************************************************
@@ -96,8 +91,16 @@ class Person {
  *
  * new vendor = new Vendor(name, x, y);
  **********************************************************/
-class Vendor {
-  // implement Vendor!
+class Vendor extends Person {
+  range = 5;
+  price = 1;
+
+  sellTo = (customer, numberOfIceCreams) => {
+    this.moveTo(customer.location);
+    const cost = this.price * numberOfIceCreams;
+    customer.wallet.debit(cost);
+    this.wallet.credit(cost);
+  };
 }
 
 /**********************************************************
@@ -116,14 +119,151 @@ class Vendor {
  *
  * new customer = new Customer(name, x, y);
  **********************************************************/
-class Customer {
-  // implement Customer!
+class Customer extends Person {
+  constructor(name, x, y) {
+    super(name, x, y);
+    this.wallet.credit(10);
+  }
+
+  _isInRange = (vendor) =>
+    this.location.distanceTo(vendor.location) <= vendor.range;
+
+  _haveEnoughMoney = (vendor, numberOfIceCreams) =>
+    this.wallet.money >= vendor.price * numberOfIceCreams;
+
+  requestIceCream = (vendor, numberOfIceCreams) => {
+    if (
+      this._isInRange(vendor) &&
+      this._haveEnoughMoney(vendor, numberOfIceCreams)
+    ) {
+      vendor.sellTo(this, numberOfIceCreams);
+    }
+  };
 }
 
 export { Point, Wallet, Person, Customer, Vendor };
 
-/***********************************************************
- * If you want examples of how to use the
- * these classes and how to test your code manually,
- * check out the README.md file
- ***********************************************************/
+
+/**************************************************************
+//  * Point: defines a point on the map using X and Y coordinates
+//  *
+//  * x: x coordinate
+//  * y: y coordinate
+//  *
+//  * distanceTo(point): takes a point, calculates the distance to
+//  *                     that point from the current point.
+//  *
+//  * let point = new Point(x, y);
+//  ****************************************************************/
+// class Point {
+//   constructor(x, y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+
+//   distanceTo = point => {
+//     let xDelta = this.x - point.x;
+//     let yDelta = this.y - point.y;
+//     return Math.sqrt(xDelta * xDelta + yDelta * yDelta); // PYTHAGORAS!
+//   };
+
+//   equals = point => point.x === this.x && point.y === this.y;
+
+//   static randomPoint = (maxX, maxY) => {
+//     let x = Math.random() * (maxX || 100);
+//     let y = Math.random() * (maxY || 100);
+//     return new Point(x, y);
+//   };
+// }
+
+// /**********************************************************
+//  * Wallet: keeps track of money
+//  *
+//  * money: how much money is in the wallet. Defaults to 0.
+//  *
+//  * credit(amount): adds `amount` to `money`.
+//  *
+//  * debit(amount): subtracts `amount` from `money`.
+//  *
+//  * let wallet = new Wallet(money);
+//  **********************************************************/
+// class Wallet {
+//   // implement Wallet!
+//   constructor(money = 0) {
+//     this.money = money;
+//   }
+
+//   credit = amount => {
+//     let moreMoney = this.money += amount ;
+//   };
+
+//   debit = amount => {
+//     let deducts = this.money -= amount;
+//   };
+// }
+
+// /**********************************************************
+//  * Person: defines a person with a name (and feelings)
+//  *
+//  * name: name of said person
+//  * location: a Point
+//  * wallet: a Wallet instance initially with 0.
+//  *
+//  * moveTo(point): updates the `location` to `point`
+//  *
+//  * let person = new Person(name, x, y);
+//  **********************************************************/
+// class Person {
+//   // implement Person!
+//   constructor(name, x, y){
+//     this.name = name;
+//     this.location = new Point();
+//     this.wallet = new Wallet(money=0);
+//   }
+
+//   // locaton = new Point() 
+
+//   // wallet = new Wallet(money = 0);
+
+// }
+
+// /**********************************************************
+//  * Vendor: defines a vendor
+//  * Subclasses Person
+//  *
+//  * range: the maximum distance this vendor can travel - initially 5
+//  * price: the cost of a single ice cream - initially 1
+//  *
+//  * sellTo(customer, numberOfIceCreams):  sells a specific number of ice creams
+//  *     to the customer by doing the following:
+//  *         - Moves to the customer's location
+//  *         - Transfers money from the customer's wallet
+//  *           to the vendor's wallet
+//  *
+//  * new vendor = new Vendor(name, x, y);
+//  **********************************************************/
+// class Vendor {
+//   // implement Vendor!
+// }
+
+// /**********************************************************
+//  * Customer: defines a customer
+//  * Subclasses Person
+//  *
+//  * wallet: a Wallet instance initially with 10.
+//  *
+//  * _isInRange(vendor): checks if the customer is in range of vendor.
+//  *
+//  * _haveEnoughMoney(vendor, numberOfIceCreams): checks if the customer
+//  *     has enough money to buy a specific number of ice creams from vendor.
+//  *
+//  * requestIceCream(vendor, numberOfIceCreams): if the customer is in the vendor's
+//  *     range and has enough money for ice cream, a request is sent to the vendor.
+//  *
+//  * new customer = new Customer(name, x, y);
+//  **********************************************************/
+// class Customer {
+//   // implement Customer!
+// }
+
+// export { Point, Wallet, Person, Customer, Vendor };
